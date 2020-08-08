045a7d3bbb8d2da1f16601980970a82191136318
Debashish Hota <debashish.hota@BRILLIO.COM>
2020-08-08 16:21:46 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {name = "zxz", age=30},
+      {name = "rfr", age=40}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Learning React</h1>
+        <button>Switch Name</button>
+        <Person name="abc" age="20">My Hobbies : Studying</Person>
+        <Person name="def" age="30"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
